09486fa93cf3e9f2c3ee6261d677150ba426f7d0
Ulises Chavarria <uchavarria@scu.edu>
2025-02-26 21:23:59 -0800

    ran into memory error attempting to fix

--- knee-osteo.py
+++ knee-osteo.py
@@ -211,7 +211,17 @@ test_gen_new = ts_gen.flow_from_dataframe(
     batch_size=batch_size,
 )
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+gpus = tf.config.list_physical_devices("GPU")
+print("Num GPUs Available: ", len(gpus)
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        print("GPU memory growth set to True")
+    except RuntimeError as e:
+        print(e)
+else:
+    print("No GPU available")
 
 # %%
 import tensorflow as tf
